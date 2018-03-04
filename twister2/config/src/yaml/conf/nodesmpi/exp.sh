8619f25055a5782bfc749d84fccd75c1385a8378
supunkamburugamuva <supun06@gmail.com>
2018-03-04 10:19:28 -0500

    catching excetions that can be coming from

--- twister2/config/src/yaml/conf/nodesmpi/exp.sh
+++ twister2/config/src/yaml/conf/nodesmpi/exp.sh
@@ -7,7 +7,8 @@ if [ $em != $2 ]; then
 fi
 
 if [ $OMPI_COMM_WORLD_RANK = "0" ]; then
-    echo "Rank 0"
+op=-agentpath:/home/supun/tools/jprofiler7/bin/linux-x64/libjprofilerti.so=port=8849,nowait
 fi
+op=""
 
-java -Xmx2048m -Xms2048m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djava.util.logging.config.file=nodesmpi/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.mpi.MPIProcess --container_class $4 --job_name $5 --twister2_home $6 --cluster_type nodesmpi --config_dir $7 &
+java $op -Xmx4096m -Xms4096m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djava.util.logging.config.file=nodesmpi/logger.properties $cp -cp $3 edu.iu.dsc.tws.rsched.schedulers.mpi.MPIProcess --container_class $4 --job_name $5 --twister2_home $6 --cluster_type nodesmpi --config_dir $7 &
